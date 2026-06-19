1b672ec187fb83bd8bd99b8ea0455642c835796a
Randrianarison Eddou <eddourandria@gmail.com>
2026-06-19 06:34:28 +0300

    refactorisation code

--- src/hooks/import/fichier1_test/features/hooks/useImportFichierTest.ts
+++ src/hooks/import/fichier1_test/features/hooks/useImportFichierTest.ts
@@ -1,7 +1,5 @@
 import type { colonneCSV } from "../../../../../types/import/fichier";
-import { obtenirDateFormatee, type_cout_mapping, useConsts } from "../../../../costs/useCosts";
-import { useCostTicketsGLPI } from "../../../../costs/useCostTicketsGLPI";
-import { TicketServiceFront } from "../../../../FrontOffice/tickets/useCreateTickets";
+import { TraiteTickets, type traitementTickets } from "../../../../tickets/useTickets";
 import { importCache } from "../service/importCaches";
 
 export interface csvfichier4 {
@@ -19,93 +17,7 @@ export const donner: Record<string, number> = {
 };
 
 export function useImportTest() {
-    const { getCostByTickets } = useCostTicketsGLPI();
-    const { getByTickets, upsert, RemoveForce, getByTicketsFirst, getByTicketsAll, getByTicketsAllTotal } = useConsts();
-
-    const traiterLigneTicket = async (idTickets: number, row: colonneCSV["fichier4"]) => {
-        const dateAujourdhui = obtenirDateFormatee();
-        console.log("id : " + idTickets);
-        const valeur = Number(row.valeur);
-        console.log("cout csv :", valeur);
-        const mode = Number(row.mode);
-
-        const relations = await TicketServiceFront.getLinkedItems(idTickets);
-        const reelGLPI = await getCostByTickets(idTickets);
-        const coutTotalGLPI = reelGLPI?.cost_Total || 0;
-
-        const totalItems = relations.length > 0 ? relations.length : 1;
-
-        const prixParItems = valeur / totalItems;
-        const prixParItemsGLPI = coutTotalGLPI / totalItems;
-
-        if (row.mvt === "open") {
-            let reelCost = 0;
-            if (mode === 2) {
-                const firstSuperCost = await getByTicketsFirst(idTickets, type_cout_mapping.SUPER_COST, totalItems);
-                reelCost = Number(firstSuperCost?.cost || 0);
-            }
-            else if (mode === 3) {
-                const MoyenneSuperCost = await getByTicketsAll(idTickets, type_cout_mapping.SUPER_COST, totalItems);
-                const total = await getByTicketsAllTotal(idTickets, type_cout_mapping.SUPER_COST, totalItems);
-                reelCost = Number(MoyenneSuperCost?.cost || 0) / Number(total || 1);
-                console.log("total Super cost "+MoyenneSuperCost?.cost);
-                console.log("super Cost diviseur :"+total);
-                console.log("mode 3:" + reelCost);
-
-            }
-            else if (mode === 4) {
-                const allSuperCost = await getByTicketsAll(idTickets, type_cout_mapping.SUPER_COST, totalItems);
-                reelCost = Number(allSuperCost?.cost || 0);
-            }
-            else {
-                const dernierSuperCost = await getByTickets(idTickets, type_cout_mapping.SUPER_COST, totalItems);
-                reelCost = dernierSuperCost?.cost || 0;
-            }
-
-            const ouverture = (valeur * Number(reelCost)) / 100;
-            const parItem = ouverture / totalItems;
-
-            console.log("items:", totalItems);
-            console.log("Open et ouverture global:", ouverture);
-            console.log("Open et ouverture par items:", parItem);
-
-            if (relations.length > 0) {
-                for (const item of relations) {
-                    await upsert(idTickets, parItem, type_cout_mapping.OUVERTURE, item.itemtype, item.items_id,dateAujourdhui);
-                }
-            } else {
-                await upsert(idTickets, parItem, type_cout_mapping.OUVERTURE, "Réouverture globale", null,dateAujourdhui);
-            }
-            await TicketServiceFront.updateStatus(idTickets, 2);
-        }
-        else if (row.mvt === "cancel") {
-            console.log("cancel avec total Items cancel: " + totalItems);
-            await RemoveForce(idTickets, type_cout_mapping.SUPER_COST, totalItems);
-            await TicketServiceFront.updateStatus(idTickets, 2);
-        }
-        else {
-            try {
-                console.log("cout csv :", valeur);
-                console.log("cout reel GLPI par items :", prixParItemsGLPI);
-
-                if (relations.length > 0) {
-                    for (const item of relations) {
-                        await upsert(idTickets, prixParItems, type_cout_mapping.SUPER_COST, item.itemtype, item.items_id,dateAujourdhui);
-                        await upsert(idTickets, prixParItemsGLPI, type_cout_mapping.GLPI, item.itemtype, item.items_id,dateAujourdhui);
-                    }
-                } else {
-                    await upsert(idTickets, valeur, type_cout_mapping.SUPER_COST, "", null,dateAujourdhui);
-                    await upsert(idTickets, coutTotalGLPI, type_cout_mapping.GLPI, "", null,dateAujourdhui);
-                }
-                await TicketServiceFront.updateStatus(idTickets, 6, "Clôture via import CSV");
-            } catch (error: any) {
-                console.error("Erreur lors de la clôture des coûts : " + error.message);
-                alert("Erreur lors du calcul ou de l'enregistrement des coûts.");
-            }
-        }
-        console.log("------------------------------");
-    };
-
+    const {traiterLigneTicket} = TraiteTickets();
     const Importer = async (rows: colonneCSV["fichier4"][]) => {
         if (rows.length === 0) return;
         console.log(JSON.stringify(rows, null, 2));
@@ -113,7 +25,14 @@ export function useImportTest() {
         for (const row of rows) {
             const glpiId = importCache.ticket.get(String(row.Tickets).trim());
             const idTickets = Number(glpiId);
-            await traiterLigneTicket(idTickets, row);
+            const mode = Number(row.mode);
+            const rowReel : traitementTickets = {
+                Tickets : row.Tickets,
+                mvt : row.mvt,
+                valeur : String(row.valeur),
+            }
+            // await traiterLigneTicket(idTickets, row);
+            await traiterLigneTicket(idTickets,rowReel ,mode );
         }
         console.log("vita tompoko");
     };
