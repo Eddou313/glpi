c5d16ee7d91453678eb992e61aa0fd14e357cce1
tonioluc <antoniorandria3342@gmail.com>
2026-06-22 07:44:50 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-      '/glpi-api': {
-        target: process.env.VITE_SERVER_DOMAINE || 'http://glpi.local',
-        changeOrigin: true,               
-        // rewrite: (path) => path.replace(/^\/glpi-api/, '/api'),
-        rewrite: (path) => path.replace(/^\/glpi-api/, '/api.php')
-        // Si votre GLPI est en HTTPS avec un cert auto-signé, décommentez :
-        // secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const glpiTarget = env.VITE_SERVER_DOMAINE || 'http://glpi.localhost'
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': 'http://localhost:3000',
+        '/glpi-api': {
+          target: glpiTarget,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/glpi-api/, '/api.php'),
+          // Si votre GLPI est en HTTPS avec un cert auto-signé, décommentez :
+          // secure: false,
+        },
       },
-    }
+    },
   }
 });
